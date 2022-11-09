ba5e5c12cedbafd20acc5407d559dcb9367c68e3
George Okinda <sinosoftkenya@gmail.com>
2022-11-09 15:40:58 +0300

    add vendor folder

--- index.php
+++ index.php
@@ -13,10 +13,10 @@ define('LARAVEL_START', microtime(true));
 | If the application is in maintenance / demo mode via the "down" command
 | we will load this file so that any pre-rendered content can be shown
 | instead of starting the framework, which could cause an exception.
-|git 
+|
 */
 
-if (file_exists($maintenance = __DIR__.'/./storage/framework/maintenance.php')) {
+if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
